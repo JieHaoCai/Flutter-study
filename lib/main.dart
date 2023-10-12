ef05976b4178148517cfce343ebe69c04ecd22a1
蔡杰浩 <tsai@caijiehaodeMacBook-Air.local>
2023-10-12 20:59:26 +0800

    add

--- lib/main.dart
+++ lib/main.dart
@@ -3,61 +3,61 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Card卡片'),
-        ),
-        body: const Column(children: [
-          MyApp(),
-        ])),
+      appBar: AppBar(
+        title: const Text('这是一个基础的页面标题'),
+      ),
+      body: const MyApp(),
+    ),
   ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    //页面显示一个容器，宽度是屏幕的宽度  高度是容器宽度的一半
-    // return AspectRatio(
-    //   aspectRatio: 2 / 1,
-    //   child: Container(
-    //     color: Colors.red,
-    //   ),
-    // );
+  List<Widget> _initCard() {
+    List<Widget> list = [];
 
-    List<Widget> _initCard() {
-      List<Widget> list = [];
-      for (var i = 0; i < 20; i++) {
-        list.add(
-          const Card(
-            margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
-            elevation: 10, //阴影
-            child: Column(
-              children: [
-                ListTile(
-                  title: Text(
-                    '张三',
-                    style: TextStyle(fontSize: 28),
-                  ),
-                  subtitle: Text('高级软件工程师'),
-                ),
-                Divider(),
-                ListTile(
-                  title: Text('电话:1111111111111111111'),
-                ),
-                ListTile(
-                  title: Text('地址：xxxxxxxxxxxx'),
-                ),
-              ],
-            ),
+    for (var i = 0; i < 100; i++) {
+      list.add(Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        //给他的四周加一个间距
+        elevation: 20,
+        margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+        child: Column(children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Image.network(
+                "https://p5.img.cntv.cn/photoAlbum/page/performance/img/2023/10/12/1697067789970_707.jpg"),
           ),
-        );
-      }
-      return list;
+          ListTile(
+            leading:
+                // ClipOval(
+                //   child: Image.network(
+                //     'https://static.bliiblii.com/static-m/hz-active/img/giftBg.png',
+                //     fit: BoxFit.cover,
+                //   ),
+                // ),
+                const CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(
+                  "https://static.bliiblii.com/static-m/hz-active/img/giftBg.png"),
+            ),
+            title: Text('标题 $i'),
+            subtitle: Text('这是一个简介 $i'),
+          )
+        ]),
+      ));
     }
 
-    //卡片效果
+    return list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return ListView(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
       children: _initCard(),
     );
   }
